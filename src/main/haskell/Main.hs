ac3b7c491d0476e206d142ed1df96db052ae06df
Alexander Nemish <anemish@newground.com.ua>
2017-02-02 16:51:34 +0200

    added basic getopts

--- src/main/haskell/Main.hs
+++ src/main/haskell/Main.hs
@@ -23,8 +23,33 @@ import System.IO
 import System.Environment
 import System.Process
 import System.Console.Haskeline
+import Options.Applicative
 
 import qualified LLVM.General.AST as AST
+
+data LascaOpts = LascaOpts
+  { lascaFiles :: [String]
+  , exec :: Bool
+  , printLLVMAsm :: Bool
+  }
+
+sample :: Parser LascaOpts
+sample = LascaOpts
+  <$> some (argument str (metavar "FILES..."))
+  <*> switch
+      ( long "exec"
+     <> help "Execute immediatly" )
+  <*> switch
+      ( long "print-llvm"
+      <> help "Execute immediatly" )
+
+greet :: LascaOpts -> IO ()
+greet (LascaOpts [] _ _) = repl
+greet (LascaOpts files exec printLlvm) = do
+    if exec
+    then mapM (execFile) files
+    else mapM processFile files
+    return ()
 
 initModule :: String -> AST.Module
 initModule name = emptyModule name
@@ -55,20 +80,31 @@ readMod fname = do
   file <- readFile fname
   genModule (initModule fname) file
 
+execFile :: String -> IO ()
+execFile fname = do
+  mod <- readMod fname
+  putStrLn "Read module OK"
+  case mod of
+    Just mod -> do
+        runJIT mod
+        return ()
+    Nothing -> do
+        putStrLn "Couldn't compile a module"
+
 processFile :: String -> IO ()
 processFile fname = do
   mod <- readMod fname
   putStrLn "Read module OK"
   case mod of
     Just mod -> do
-        runJIT mod
---         Just(asm) <- getLLAsString mod
---         writeFile (fname ++ ".ll") asm
---         let name = dropWhile (/= '.') fname
---         callProcess "gcc" ["-o", name, fname ++ ".ll"]
+        Just(asm) <- getLLAsString mod
+        writeFile (fname ++ ".ll") asm
+        let name = dropWhile (/= '.') fname
+        callProcess "clang-3.5" ["-o", name, "-L.", "-llascart", fname ++ ".ll"]
         return ()
     Nothing -> do
         putStrLn "Couldn't compile a module"
+
 
 
 repl :: IO ()
@@ -86,7 +122,15 @@ repl = runInputT defaultSettings (loop (initModule "Lasca JIT"))
           Nothing -> loop mod
 
 main :: IO ()
-main = do
+main = execParser opts >>= greet
+  where
+    opts = info (helper <*> sample)
+      ( fullDesc
+     <> progDesc "Print a greeting for TARGET"
+     <> header "hello - a test for optparse-applicative" )
+
+main1 :: IO ()
+main1 = do
   args <- getArgs
   case args of
     []      -> repl
